38dbfb99e862cbaa8eb7d0cec90567c147ba24d0
a1573595 <a1573595@gmail.com>
2020-02-13 23:40:01 +0800

    Set screen orientation portrait

--- app/src/main/java/com/example/puffer/parkingdemo/parkList/ParkListActivity.java
+++ app/src/main/java/com/example/puffer/parkingdemo/parkList/ParkListActivity.java
@@ -13,7 +13,6 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
